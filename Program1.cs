e60260e1335c18b629d3c5d8298b5dbe31854983
Anastasia <getupnastya2000@gmail.com>
2019-11-18 18:33:39 +0300

    Helloy Word! my name is C#

---
+++ Program1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace output_on_display
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! My name is C#");
+            Console.ReadLine(); 
+        }
+    }
+}
